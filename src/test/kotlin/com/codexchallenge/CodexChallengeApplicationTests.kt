fa83b3e115a159b099c5fef7ae26d5ea2f315450
Alex Rosa <alexr@gec.inatel.br>
2023-10-05 15:04:48 -0300

    Stater code for the challenge - setup springboot project

---
+++ src/test/kotlin/com/codexchallenge/CodexChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.codexchallenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodexChallengeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
