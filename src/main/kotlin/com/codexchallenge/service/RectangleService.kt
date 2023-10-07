5816d062c188644f396e5c3e336391e90f7acb3c
Alex Rosa <alexr@gec.inatel.br>
2023-10-07 15:22:31 -0300

    Added method to compute intersection area and refactoring RectangleService class

--- src/main/kotlin/com/codexchallenge/service/RectangleService.kt
+++ src/main/kotlin/com/codexchallenge/service/RectangleService.kt
@@ -2,8 +2,10 @@ package com.codexchallenge.service
 
 import com.codexchallenge.model.AreaResponseModel
 import com.codexchallenge.model.IntersectionResponseModel
+import com.codexchallenge.model.RectangleModel
 import com.codexchallenge.model.RectanglesPointsModel
 import org.springframework.stereotype.Service
+import java.awt.Point
 import kotlin.math.max
 import kotlin.math.min
 
@@ -15,7 +17,7 @@ class RectangleService {
     }
 
     fun checkRectangleIntersection(rectanglesPoints: RectanglesPointsModel): IntersectionResponseModel {
-        return IntersectionResponseModel(intersectionDetected = hasIntersection(rectanglesPoints))
+        return IntersectionResponseModel(hasIntersection(getResultingRectangle(rectanglesPoints)))
     }
 
     fun calculateIntersectionArea(rectanglesPoints: RectanglesPointsModel): AreaResponseModel {
@@ -25,11 +27,24 @@ class RectangleService {
 }
 
 fun calculateArea(rectanglesPoints: RectanglesPointsModel): AreaResponseModel {
-    //
-    return AreaResponseModel()
+    //check if exist intersection before compute area
+    val rectangle = getResultingRectangle(rectanglesPoints)
+    if (!hasIntersection(rectangle)) {
+        return AreaResponseModel(false, 0)
+    }
+
+    //calculate the area do Retangulo resultante
+    //#height: p2y - p1y     #base: p2x - p1x    #plus 1 to INCLUDED points
+    val area: Int
+    rectangle.apply {
+        area = (p2.y - p1.y + 1) * (p2.x - p1.x + 1)
+    }
+
+    println(area)
+    return AreaResponseModel(true, area)
 }
 
-fun hasIntersection(rectanglesPoints: RectanglesPointsModel): Boolean{
+fun getResultingRectangle(rectanglesPoints: RectanglesPointsModel): RectangleModel {
     //Rectangle A points
     val x1: Int = rectanglesPoints.rec1.p1.x
     val y1: Int = rectanglesPoints.rec1.p1.y
@@ -44,19 +59,25 @@ fun hasIntersection(rectanglesPoints: RectanglesPointsModel): Boolean{
     val y4 = rectanglesPoints.rec2.p2.y
     println("B = p1($x3, $y3) - p2($x4, $y4)")
 
-    //Math definition to identity rectangles intersection
+    //Math definition to get the points of a possible intersection area
     val x5 = max(x1, x3)
     val y5 = max(y1, y3)
     val x6 = min(x2, x4)
     val y6 = min(y2, y4)
     println("Retangulo resultante = p1($x5, $y5), p2($x6, $y6)")
+    return RectangleModel(Point(x5, y5), Point(x6, y6))
+}
 
-    // no intersection condition
-    return if (x5 > x6 || y5 > y6) {
-        println("No intersection")
-        false
-    } else {
-        println("Intersection Detected!!!")
-        true
+fun hasIntersection(rectangle: RectangleModel): Boolean {
+    // if (x5 > x6 || y5 > y6) println("No intersection")
+    //Math definition to check for intersection
+    rectangle.apply {
+        return if (p1.x > p2.x || p1.y > p2.y) {
+            println("No intersection")
+            false
+        } else {
+            println("Intersection Detected!!!")
+            true
+        }
     }
 }
